1a8192d5ff10973bfdb28c5fd64d538eb0401e69
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-05-31 13:45:03 +0200

    asdasda

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ export default defineConfig({
   redirects: {
     '/redesign': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/nl/Educational-Program-2024-2025.aspx',
     '/LO': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/Competentieprofiel(1).aspx',
-    '/LU': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/nl/Competentieprofiel(1).aspx'
+    '/lo': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/Competentieprofiel(1).aspx',
+    '/LU': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/nl/Competentieprofiel(1).aspx',
+    '/lu': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/nl/Competentieprofiel(1).aspx'
   }
 });
