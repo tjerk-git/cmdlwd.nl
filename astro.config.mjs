5fbac92091e83e47ea9722df6a1afe5961e607d2
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-05-31 13:42:09 +0200

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    '/redesign': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/nl/Educational-Program-2024-2025.aspx'
+    '/redesign': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/nl/Educational-Program-2024-2025.aspx',
+    '/LO': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/Competentieprofiel(1).aspx',
+    '/LU': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/nl/Competentieprofiel(1).aspx'
   }
 });
