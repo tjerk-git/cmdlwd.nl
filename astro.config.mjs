fbcf93736c5d1367e04b5f56c43e4f8694c3d030
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-05-31 13:14:59 +0200

    push

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  redirects: {
+    '/redesign': 'https://newuniversity.sharepoint.com/sites/CMDLWD/SitePages/nl/Educational-Program-2024-2025.aspx'
+  }
+});
